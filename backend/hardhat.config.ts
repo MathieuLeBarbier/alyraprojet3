20ca10e5f2204346392709a5e58d0cce77952f3d
Maxime Normandin <1720380@cstjean.qc.ca>
2025-06-23 23:24:55 -0400

    fix git action test to allow conditional networks

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -3,6 +3,7 @@ import "@nomicfoundation/hardhat-toolbox";
 import * as dotenv from "dotenv";
 
 dotenv.config();
+
 // Sepolia
 const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY || "";
 const SEPOLIA_URL = process.env.SEPOLIA_RPC_URL || "";
@@ -17,16 +18,24 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks: {
-    sepolia: {
-      url: SEPOLIA_URL,
-      accounts: [`0x${SEPOLIA_PRIVATE_KEY}`],
-      chainId: 11155111,
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
     },
-    holesky: {
-      url: HOLESKY_URL,
-      accounts: [`0x${HOLESKY_PRIVATE_KEY}`],
-      chainId: 17000,
-    },
+    ...(SEPOLIA_PRIVATE_KEY && SEPOLIA_URL ? {
+      sepolia: {
+        url: SEPOLIA_URL,
+        accounts: [`0x${SEPOLIA_PRIVATE_KEY}`],
+        chainId: 11155111,
+      }
+    } : {}),
+    ...(HOLESKY_PRIVATE_KEY && HOLESKY_URL ? {
+      holesky: {
+        url: HOLESKY_URL,
+        accounts: [`0x${HOLESKY_PRIVATE_KEY}`],
+        chainId: 17000,
+      }
+    } : {}),
   },
   etherscan: {
     apiKey: {
@@ -37,3 +46,4 @@ const config: HardhatUserConfig = {
 };
 
 export default config;
+
